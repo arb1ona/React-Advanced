d03445fc792f734a9432e4cab64c3d416863a018
arb1ona <arbiona.shahu@gmail.com>
2021-02-16 21:58:28 +0100

    create component

--- Trial.js
+++ Trial.js
@@ -1,12 +1,11 @@
 import "./app.css";
-import Header from "./components/Header";
+
 function App() {
   const name = "Arbiona";
   const x = false;
 
   return (
     <div className="container">
-      <Header />
       <p>
         {1 + 2 + 3}
         <br />
