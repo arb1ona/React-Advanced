0503439665757c2446759b4cf0f9a123ccdef784
arb1ona <arbiona.shahu@gmail.com>
2021-02-16 23:12:44 +0100

    create a list with .map()

---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+  { id: 1, text: "Clean up the makeup", day: "Feb 16th at 10:00pm" },
+  { id: 2, text: "Check the email inbox", day: "Feb 16th at 12:00pm" },
+  { id: 3, text: "Prepare lunch", day: "Feb 16th at 1:30pm" },
+  { id: 4, text: "Go out for a walk", day: "Feb 16th at 7:00pm" },
+];
+
+const Tasks = () => {
+  return (
+    <>
+      {tasks.map((item) => (
+        <h3 key={item.id}>{item.text}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
