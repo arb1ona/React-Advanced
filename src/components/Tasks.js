bff13eddd3a3ce67b61a38b0ae0a58d340b5514c
arb1ona <arbiona.shahu@gmail.com>
2021-02-16 23:20:52 +0100

    State & useState Hook

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,13 @@
-const tasks = [
-  { id: 1, text: "Clean up the makeup", day: "Feb 16th at 10:00pm" },
-  { id: 2, text: "Check the email inbox", day: "Feb 16th at 12:00pm" },
-  { id: 3, text: "Prepare lunch", day: "Feb 16th at 1:30pm" },
-  { id: 4, text: "Go out for a walk", day: "Feb 16th at 7:00pm" },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, settasks] = useState([
+    { id: 1, text: "Clean up the makeup", day: "Feb 16th at 10:00pm" },
+    { id: 2, text: "Check the email inbox", day: "Feb 16th at 12:00pm" },
+    { id: 3, text: "Prepare lunch", day: "Feb 16th at 1:30pm" },
+    { id: 4, text: "Go out for a walk", day: "Feb 16th at 7:00pm" },
+  ]);
+
   return (
     <>
       {tasks.map((item) => (
